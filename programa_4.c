b7ae9883f9cb6b59d5ad578767f84aad8deea98b
Yuliza18 <syestupinanp@udistrital.edu.co>
2025-10-21 13:16:59 -0500

    Add files via upload

---
+++ programa_4.c
@@ -0,0 +1,25 @@
+//programa_4.c
+// Yuliza Estupiñan ponce 
+// 21 de octubre del 2025
+// Almacenamiento de datos  en variable 
+#include <stdio.h>
+int main()
+{
+int entero;
+float flotante;
+printf("Ingresa el valor entero: ");
+scanf("%i", &entero);
+printf("El valor ingresado es: %d\0", entero);
+printf("Ingresa el valor float: ");
+scanf("%f", &flotante);
+printf("El valor ingresado es: %f\0", flotante);
+return 0;
+}
+// Cuando esta el caracter n genera salto de linea 
+// cuando se coloca la a emite un sonido 
+//La  letra r hace que el curso vuelva a  el inicio de  la linea 
+// La  letra b reduce  una posicion hacia atras 
+// Cuando colocamos  la letra f cambia de pagina 
+// La letra t inserta un espacio en blanco que mueve el cursor a la siguiente posiciòn de tabulaciòn 
+//La letra V nos desplaza al texto verticalmente 
+// El numero 0  indica el final del codigo 

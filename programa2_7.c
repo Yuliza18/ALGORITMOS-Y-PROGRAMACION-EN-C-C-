f75ed0ae5f5ccfd3d3527e830aa397fb51cf9642
Yuliza18 <syestupinanp@udistrital.edu.co>
2025-10-24 13:16:11 -0500

    Add files via upload

---
+++ programa2_7.c
@@ -0,0 +1,23 @@
+// Yuliza Estupiñan 
+// 24 de octubre del 2025 
+// comprueba que las condiciones son numéricas
+
+#include <stdio.h>
+int main()
+{
+if (0)
+{
+
+ // Esta instrucción nunca se ejecuta
+printf("Esta instrucción nunca se ejecuta\n");
+
+// porque la condición siempre es falsa (0).
+printf("porque la condición siempre es falsa (0).\n");
+}
+if (80) // El bloque de código de esta estructura if
+// solo consta de una línea porque los comentarios
+// no son tomados en cuenta por el compilador.
+// La condición siempre es verdadera (diferente de 0)
+printf("Esta instrucción siempre se ejecuta.\n");
+return 0;
+}

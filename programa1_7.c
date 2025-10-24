0aa00c530f1097d81d37fa1cd8c814de121307b8
Yuliza18 <syestupinanp@udistrital.edu.co>
2025-10-24 13:03:47 -0500

    Add files via upload

---
+++ programa1_7.c
@@ -0,0 +1,24 @@
+ // Yuliza Estupiñan ponce 
+// 24 de octubre del 2025 
+// Estructuras de selección 
+#include <stdio.h>  //libreria
+
+int main() {
+    int a, b;
+ printf ( " ingrese el valor de a:"); 
+scanf("%d" , &a);  
+printf ("ingrse el valor de b: ");
+    scanf("%d" , &b);  
+
+// Comprobamos que si a es mayor que b 
+
+    if (a > b) //Evaluo si a es mayor que b 
+{
+        printf("\ta (%d) es mayor que b (%d).\n", a, b);// si se cumple mostrar el numero 
+    }
+// mensaje final 
+    printf("\tEl programa sigue su flujo.\n");
+
+    return 0;
+}
+
